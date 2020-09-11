9e7dbb07dd40f43b4ff0110b0f1e35182cf78b4d
airson <airson_yu@163.com>
2020-09-11 17:47:16 +0800

    CollectionOverview

---
+++ src/main/java/com/lhfeiyu/tech/java/collection/CollectionOverview.java
@@ -0,0 +1,30 @@
+package com.lhfeiyu.tech.java.collection;
+
+import java.util.*;
+
+public class CollectionOverview {
+
+    public static void main(String[] args) {
+        demo();
+    }
+
+    public static void demo() {
+
+        List<String> list = new ArrayList<>();
+        Set<String> set = new HashSet<>();
+        Map<String, String> map = new HashMap<>();
+
+        list.add("aaa");
+        list.add("bbb");
+        list.add("aaa");
+        set.add("aaa");
+        set.add("bbb");
+        set.add("aaa");
+        map.put("aaa", "1");
+        map.put("aaa", "1");
+        map.put("bbb", "1");
+
+
+    }
+
+}
