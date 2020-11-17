64d8ef45fcd1be0ee93a07e4557018b99857a141
airson <airson_yu@163.com>
2020-11-17 15:15:58 +0800

    Base64Demo

---
+++ src/main/java/com/lhfeiyu/tech/java/base/Base64Demo.java
@@ -0,0 +1,35 @@
+package com.lhfeiyu.tech.java.base;
+
+import org.apache.ibatis.javassist.bytecode.ByteArray;
+
+import java.nio.ByteBuffer;
+import java.text.MessageFormat;
+import java.util.Base64;
+import java.util.Calendar;
+import java.util.Formatter;
+import java.util.Locale;
+
+/**
+ * @author airson
+ */
+public class Base64Demo {
+
+    public static void main(String[] args) {
+        formatString();
+        //nullCheck();
+    }
+
+    public static void formatString() {
+        int n = 0x0f03<<16;
+        byte[] b = new byte[4];
+        b[3] = (byte) (n & 0xff);
+        b[2] = (byte) (n >> 8 & 0xff);
+        b[1] = (byte) (n >> 16 & 0xff);
+        b[0] = (byte) (n >> 24 & 0xff);
+
+        String str = Base64.getEncoder().encodeToString(b);
+        System.out.println(str);
+
+    }
+
+}
