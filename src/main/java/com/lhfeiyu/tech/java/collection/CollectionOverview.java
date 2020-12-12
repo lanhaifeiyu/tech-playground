87ed8903b241508e3a4dbcbc51f83ae5110225b7
airson <airson_yu@163.com>
2020-12-12 11:13:00 +0800

    conflict fix

--- src/main/java/com/lhfeiyu/tech/java/collection/CollectionOverview.java
+++ src/main/java/com/lhfeiyu/tech/java/collection/CollectionOverview.java
@@ -2,21 +2,11 @@ package com.lhfeiyu.tech.java.collection;
 
 import java.util.*;
 
-<<<<<<< HEAD
 /**
  * TODO
  *
  * @author airson
  */
-public class CollectionOverview {
-
-    public static void main(String[] args) {
-        List<String> userList = new ArrayList<>();
-        Set<String> userSet = new HashSet<>();
-        Map<String,String> userMap = new HashMap<>();
-    }
-
-=======
 public class CollectionOverview {
 
     public static void main(String[] args) {
@@ -39,8 +29,6 @@ public class CollectionOverview {
         map.put("aaa", "1");
         map.put("bbb", "1");
 
-
     }
->>>>>>> origin/master
 
 }
