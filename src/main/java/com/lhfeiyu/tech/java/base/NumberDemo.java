0a57c4b270e4677e46af72039b267c0e51716c31
airson <airson_yu@163.com>
2021-01-20 18:15:04 +0800

    update eureka config and spring-boot:2.4.2,spring-cloud:2020.0.0

---
+++ src/main/java/com/lhfeiyu/tech/java/base/NumberDemo.java
@@ -0,0 +1,23 @@
+package com.lhfeiyu.tech.java.base;
+
+/**
+ * @author airson
+ */
+public class NumberDemo {
+
+    public static void main(String[] args) {
+        //formatString();
+        number_mod();
+    }
+
+    public static void number_mod() {
+        int a = 202011;
+        int b = 202011 % 100;
+
+
+        System.out.println("module:" + b);
+
+
+    }
+
+}
